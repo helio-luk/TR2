f514d383e85811ee48ba41f5ff6d4ee54bf4ba90
helio-luk <helio.ssjr@gmail.com>
2018-11-30 13:27:04 -0200

    main

---
+++ TR2/src/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include "Servidor.h"
+
+#define PORTA_DEFAULT 8228
+
+int main(int argc, char * const argv[]){
+
+	long porta;
+	if (argc != 1){
+		if(argc !=3){
+			printf("Digite: ./aracne -p <porta> \n");
+			exit(EXIT_FAILURE);
+		}else if(strcmp(argv[1], "-p") != 0){
+			printf("Digite: -p <porta> \n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	porta = strtol(argv[2], nullptr, 10);
+	if(porta < 1024 || porta > 65535){
+		printf("A porta deve estar entre 1024 e 65535 \n");
+		exit(EXIT_FAILURE);
+	}else{
+		porta = PORTA_DEFAULT;
+	}
+
+	return 0;
+
+}
