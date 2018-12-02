6af4ee92cbf93f5e7bb2f1a26bf407f92877bb5c
helio-luk <helio.ssjr@gmail.com>
2018-12-02 15:00:01 -0200

    corregar do conflito

--- TR2/src/PacoteOut.cpp
+++ TR2/src/PacoteOut.cpp
@@ -6,8 +6,10 @@
 #include<netdb.h>
 #include <iostream>
 #include <sstream>
+#include<string.h>
 
 #include "PacoteOut.hpp"
+
 
 PacoteOut::PacoteOut() = default;
 PacoteOut::~PacoteOut() = default;
@@ -17,24 +19,26 @@ struct addrinfo clienteEnd;
 struct addrinfo *cEnd;
 
 ssize_t PacoteOut::Send(HTTP::Header msg){
-	clienteEnd.ai_family = AF_INET;
-	clienteEnd.ai_socktype = INADDR_ANY;
+	memset(&clienteEnd, 0, sizeof(clienteEnd));
+	clienteEnd.ai_family = AF_UNSPEC;
+	clienteEnd.ai_socktype = 0;
 	clienteEnd.ai_flags = 0;
-	clienteEnd.ai_protocol = 0;
-
-	int info = getaddrinfo(msg.host.c_str(), msg.porta.c_str(), &clienteEnd, &cEnd);
-	if(info == 0){
-		socketOut < socket(AF_INET, SOCK_STREAM, 0);
+	clienteEnd.ai_protocol =IPPROTO_TCP;
+		int info = getaddrinfo(("http://"+msg.host).c_str(), msg.porta.c_str(), &clienteEnd, &cEnd);
+	 if(info == 0){
+		socketOut = socket(AF_INET, SOCK_STREAM, 0);
 		if(socketOut < 0){
 			printf("error ao criar socket");
 			exit(1);
 		}
 		if(connect(socketOut, cEnd->ai_addr, cEnd->ai_addrlen) < 0){
-			printf("error no socket");
+			printf("erro no socket\n");
 			exit(1);
 		}
-	}
-	ssize_t enviado = send(socketOut, msg.to_string().c_str(), msg.to_string().length(),0);
+	 }
+	 std::cout << msg.to_string() << '\n';
+	ssize_t enviado = send(socketOut, msg.to_string().c_str(), msg.to_string().length(), 0);
+
 	if(enviado < 0){
 		printf("nao foi possivel enviar dado\n");
 		exit(1);
@@ -47,12 +51,14 @@ ssize_t PacoteOut::Send(HTTP::Header msg){
 void PacoteOut::responseRecebido(){
 	int valread = 0;
 	std::string mensagem("");
+	memset(&responsesRecebidos, 0, sizeof(responsesRecebidos));
 
 	do{
 		char buffer[1024];
 		valread = static_cast<int>(read(socketOut, buffer, sizeof(buffer)));
 		mensagem += std::string(buffer, static_cast<unsigned long>(valread));
 	}while(valread == 1024);
+	printf("Response:\n%s\n",mensagem.c_str());
 	if(valread > 0){
 		responsesRecebidos.push_back(HTTP::Header(mensagem));
 	}else{
--- TR2/src/Servidor.cpp
+++ TR2/src/Servidor.cpp
@@ -6,10 +6,10 @@
 #include "Header.hpp"
 
 
-Servidor::Servidor(int numPorta) : requests(pacoteIn.requestsRecebidos)
-							, responses(pacoteOut.responsesRecebidos)
-							, pacoteIn( numPorta )
-							, pacoteOut() {}
+Servidor::Servidor(int numPorta) : requests(pIn.requestsRecebidos)
+							, responses(pOut.responsesRecebidos)
+							, pIn( numPorta )
+							, pOut() {}
 
 
 
@@ -17,39 +17,38 @@ Servidor::~Servidor() = default;
 
 void Servidor::Inicia(){
 	while(true){
-	pacoteIn.accConexao();
-	pacoteIn.getRequests();
+	pIn.accConexao();
+	pIn.getRequests();
 
-	if(!requestsRecebidos.empty())
-		std::swap( requestsRecebidos, sendRequest );
+	if(!requests.empty())
+		std::swap( requests, sendRequest );
 
 	if(!sendRequest.empty()) {
 		printf("\nEnviando %d request(s)...\n", (int) sendRequest.size());
 
 		for (auto &i : sendRequest) {
-			printf("\nEnviando request para %s:%s\n", i.host.c_str(), i.porta.c_str());
-			printf("\n%s\n", i.to_string().c_str());
-			if(pacoteOut.Send(i) == -1){
-				printf("\nNão foi possível enivar request\n");
+			//std::cout << i.to_string() << '\n';
+			//std::cout << "\nEnviando request para " << i.host.c_str()<<i.porta.c_str();
+			if(pOut.Send(i) == -1){
+				printf("\nNão foi possível enviar request\n");
 				exit(1);
 			}
 		}
-
 		sendRequest.clear();
 	}
+	pOut.responseRecebido();
+	if(!responses.empty())
+	{
+		std::swap(responses, sendResponse);
+	}
 
-	pacoteOut.responseRcv();
+	if(!sendResponse.empty()) {
+		printf("\nEnviando %d response(s)...\n", (int) sendResponse.size());
 
-	if(!responsesRecebidos.empty())
-		std::swap(responsesRecebidos, sendRequest);
-
-	if(!sendRequest.empty()) {
-		printf("\nEnviando %d response(s)...\n", (int) sendRequest.size());
-
-		for(auto &i : sendResponse) {
-			printf("\nResponse: \n%s\n", i.to_string().c_str());
-			if(pacoteIn.Send(i) == -1){
-				printf("\nNão foi possível enivar response\n");
+		for(auto &i : sendResponse) { //TÁ COM PROBLEMA AQUI
+			printf("\nResponse: \n%s\n", i.host.c_str());
+			if(pIn.Send(i) == -1){
+				printf("\nNão foi possível enviar response\n");
 				exit(1);
 			}
 		}
